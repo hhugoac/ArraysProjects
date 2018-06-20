bd879e2c1df70cdf7540bc4a510682b14afce88b
hhugoac <hhugoac@gmail.com>
2018-06-20 17:12:53 -0500

    Segunda version

--- src/com/vectores/application/AppEjercicio24.java
+++ src/com/vectores/application/AppEjercicio24.java
@@ -3,13 +3,40 @@ import com.vectores.misclases.Vectores;
 
 public class AppEjercicio24 {
 
-	public static void main(String[] args) {
-		int[] vector=new int[20];
+	public static void main(String[] args) 
+	{
+		final int NUM_ELEMENTOS=20;
+		final int PARES_IMPARES=2;
+		
+		int[] vector=new int[NUM_ELEMENTOS];
+		int[] paresimpares=new int[PARES_IMPARES];
 		Vectores objVec1=new Vectores();
 		
-		vector=objVec1.llenadoSerie(vector);
-		
+		System.out.println("Ejercicio 23. de unos");
+		vector=objVec1.llenaUnos(vector);
 		imprimirVector(vector);
+		
+		System.out.println("Ejercicio 24. de uno en uno");
+		vector=objVec1.llenadoSerie(vector);
+		imprimirVector(vector);
+		
+		System.out.println("Ejercicio 25. de dos en dos");
+		vector=objVec1.llenadoSeriedos(vector);
+		imprimirVector(vector);
+		
+		System.out.println("Ejercicio 26. Alreves");
+		vector=objVec1.llenadoSerieReves(vector);
+		imprimirVector(vector);
+		
+		System.out.println("Ejercicio 27. Aleatorio");
+		vector=objVec1.llenadoaleatorio(vector);
+		imprimirVector(vector);
+		
+		System.out.println("Ejercicio 28. Contar elementos pares e impares");
+		paresimpares=objVec1.llenadoParesImpar(vector);
+		imprimirVector(paresimpares);
+		System.out.println("La cantidad de pares es "+objVec1.contadorPares(vector));
+		System.out.println("La cantidad de pares es "+objVec1.contadorImpares(vector));
 		
 		
 	}
--- src/com/vectores/misclases/Vectores.java
+++ src/com/vectores/misclases/Vectores.java
@@ -1,7 +1,9 @@
 package com.vectores.misclases;
 
+import java.util.Random;
+
 public class Vectores {
-	
+	//Ejercicio 23
 	public int[] llenaUnos(int[] vector)
 	{
 		for(int i=0;i<vector.length;i++)
@@ -11,13 +13,105 @@ public class Vectores {
 		return vector;
 	}
 	
+	//Ejercicio 24
 	public int[] llenadoSerie(int[] vector)
 	{
 		for(int i=0;i<vector.length;i++)
 		{
-			vector[i]=i;
+			vector[i]=i+1;
 		}
 		return vector;
 	}
-
+	//Ejercicio 25
+	public int[] llenadoSeriedos(int[] vector)
+	{
+		for(int i=0;i<vector.length;i++)
+		{
+			vector[i]=2*(i+1);
+		}
+		return vector;
+	}
+	
+	//Ejercicio 26 
+	public int[] llenadoSerieReves(int[] vector)
+	{
+		for(int i=vector.length-1 ;i>=0;i--)
+		{
+			vector[vector.length-1-i]=i;
+		}
+		return vector;
+	}
+	
+	//Ejercicio 27
+	public int[] llenadoaleatorio(int[] vector)
+	{
+		Random rnd=new Random(System.nanoTime());
+		//int BOUND=100;
+		
+		for(int i=0;i<vector.length;i++)
+		{
+			vector[i]=rnd.nextInt(100);  //Agrega numero aleatorios del 1-100 
+			//vector[i]=(int)(Math.random()*BOUND)+1; 
+		}
+		return vector;
+	}
+	
+	//Ejercicio 28 
+	
+	public int[] llenadoParesImpar(int[] vector)
+	{
+		int[] paresImpares=new int[2];
+		for(int i=0;i<vector.length;i++)
+		{
+			if(vector[i]%2==0)
+			{
+				paresImpares[0]+=1;
+			}
+			else
+			{
+				paresImpares[1]+=1;
+			}
+		}
+		return paresImpares;
+	}
+	//Contados de elementos pares en un vedctor
+	public int contadorPares(int[] vector)
+	{
+		int pares=0;
+		for(int i=0;i<vector.length;i++)
+		{
+			if(vector[i]%2==0)
+			{
+				pares+=1;
+			}
+		}
+		return pares;
+	}
+	//Contador de elementos impraes en un vector
+	public int contadorImpares(int[] vector)
+	{
+		int impares=0;
+		for(int i=0;i<vector.length;i++)
+		{
+			if(vector[i]%2!=0)
+			{
+				impares+=1;
+			}
+		}
+		return impares;
+	}
+	
+	//Metodo que reotrna un vector con elementos pares de un vector
+	
+	public int[] vecPares(int[] vector)
+	{		
+		for(int i=0;i<vector.length;i++)
+		{
+			if(vector[i]%2==0)
+			{
+				Pares[i]=vector[i];
+			}
+		}
+		return Pares; 
+	}
 }
